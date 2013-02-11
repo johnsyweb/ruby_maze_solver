6e365c18f7c22a21f651fd705f123557c143fc83
Pete Johns <paj-github@johnsy.com>
2013-02-11 11:44:24 +1100

    Refactored tests

--- spec/maze_spec.rb
+++ spec/maze_spec.rb
@@ -2,31 +2,27 @@ require 'spec_helper'
 
 describe Maze do
 
-    it 'should be constructable with a string' do
-        maze = Maze::Maze.new '********'
+    def given_a_simple_8_x_3_maze
+        @maze = Maze::Maze.new('********\n' +
+                               '        \n' +
+                               '********\n'
+                              )
+
     end
 
     it 'should have a width of 8' do
-        maze = Maze::Maze.new '********'
-        maze.width.should eq 8
+        given_a_simple_8_x_3_maze
+        @maze.width.should eq 8
     end
 
     it 'should have a height of 3' do
-        maze = Maze::Maze.new('********\n' +
-                              '        \n' +
-                              '********\n'
-                             )
-
-        maze.height.should eq 3
+        given_a_simple_8_x_3_maze
+        @maze.height.should eq 3
     end
 
     it 'should be possible to visit an empty space' do
-        maze = Maze::Maze.new('********\n' +
-                              '        \n' +
-                              '********\n'
-                             )
-
-        maze.visitable?(0, 1).should be_true
+        given_a_simple_8_x_3_maze
+        @maze.visitable?(0, 1).should be_true
     end
 
 end

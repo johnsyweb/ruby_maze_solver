f8baa67a1ad57b888e3146c80285aa3b8fadb468
Pete Johns <paj-github@johnsy.com>
2013-05-19 21:16:49 +1000

    Use RSpec contexts

--- spec/maze_solver_spec.rb
+++ spec/maze_solver_spec.rb
@@ -3,115 +3,113 @@
 require 'spec_helper'
 
 describe MazeSolver do
-  def given_a_simple_8_x_3_maze
+  context 'a simple 8 x 3 maze' do
+    before do
     @maze = MazeSolver::MazeSolver.new(from_grid: <<-GRID
 ********
 
 ********
 GRID
-                                      )
-  end
+      )
+    end
 
-  it 'should have a width of 8' do
-    given_a_simple_8_x_3_maze
-    @maze.width.should eq 8
-  end
+    it 'should have a width of 8' do
+      @maze.width.should eq 8
+    end
 
-  it 'should have a height of 3' do
-    given_a_simple_8_x_3_maze
-    @maze.height.should eq 3
-  end
+    it 'should have a height of 3' do
+      @maze.height.should eq 3
+    end
 
-  it 'should be possible to visit an empty space' do
-    given_a_simple_8_x_3_maze
-    @maze.visitable?(0, 1).should be_true
-  end
+    it 'should be possible to visit an empty space' do
+      @maze.visitable?(0, 1).should be_true
+    end
 
-  it 'should not be possible to visit a wall' do
-    given_a_simple_8_x_3_maze
-    @maze.visitable?(0, 0).should be_false
-  end
+    it 'should not be possible to visit a wall' do
+      @maze.visitable?(0, 0).should be_false
+    end
 
-  it 'should not be possible to visit outside the maze' do
-    given_a_simple_8_x_3_maze
-    @maze.visitable?(8, 3).should be_false
-  end
+    it 'should not be possible to visit outside the maze' do
+      @maze.visitable?(8, 3).should be_false
+    end
 
-  it 'should not be possible to visit a visited space' do
-    given_a_simple_8_x_3_maze
-    @maze.visit 0, 1
-    @maze.visitable?(0, 1).should be_false
-  end
+    it 'should not be possible to visit a visited space' do
+      @maze.visit 0, 1
+      @maze.visitable?(0, 1).should be_false
+    end
 
-  it 'should be possible to visit an unvisited space' do
-    given_a_simple_8_x_3_maze
-    @maze.visit 0, 1
-    @maze.unvisit 0, 1
-    @maze.visitable?(0, 1).should be_true
-  end
+    it 'should be possible to visit an unvisited space' do
+      @maze.visit 0, 1
+      @maze.unvisit 0, 1
+      @maze.visitable?(0, 1).should be_true
+    end
 
-  it 'should solve a simple maze' do
-    given_a_simple_8_x_3_maze
-    @maze.solve(0, 1, 7, 1).should be_true
-  end
+    it 'should be soluble' do
+      @maze.solve(0, 1, 7, 1).should be_true
+    end
 
-  it 'should solve a simple maze backwards' do
-    given_a_simple_8_x_3_maze
-    @maze.solve(7, 1, 0, 1).should be_true
-  end
+    it 'should be soluble backwards' do
+      @maze.solve(7, 1, 0, 1).should be_true
+    end
 
-  it 'should solve a bigger maze' do
-    maze = MazeSolver::MazeSolver.new(from_grid: <<-GRID
-********
-  **   *
-* ** * *
-* *  * *
-* ** * *
-* **** *
-*      *
-********
-GRID
-                                     )
-                                     maze.solve(0, 1, 3, 3).should be_true
-  end
-
-  it 'should fail an impossible maze' do
-    maze = MazeSolver::MazeSolver.new(from_grid: <<-GRID
-********
-  **   *
-* **** *
-* *  * *
-* ** * *
-* **** *
-*      *
-********
-GRID
-                                     )
-                                     maze.solve(0, 1, 3, 3).should be_false
-  end
-
-  it 'should be printable' do
-    given_a_simple_8_x_3_maze
-    @maze.to_s.should eq <<-GRID
+    it 'should be printable' do
+      @maze.to_s.should eq <<-GRID
 ********
         
 ********
 GRID
-  end
+    end
 
-  it 'should be printable when solved' do
-    given_a_simple_8_x_3_maze
-    @maze.solve 0, 1, 7, 1
-    @maze.to_s.should eq <<-GRID
+    it 'should be printable when solved' do
+      @maze.solve 0, 1, 7, 1
+      @maze.to_s.should eq <<-GRID
 ********
 ........
 ********
-GRID
+      GRID
+    end
+
   end
 
-  it 'should be creatable from a file' do
-    maze = MazeSolver::MazeSolver.new(from_file: 'spec/test.maze')
-    maze.solve(18, 0, 12, 6).should be_true
+  context 'a bigger maze' do
+    it 'should be soluble' do
+      maze = MazeSolver::MazeSolver.new(from_grid: <<-GRID
+********
+  **   *
+* ** * *
+* *  * *
+* ** * *
+* **** *
+*      *
+********
+GRID
+      )
+      maze.solve(0, 1, 3, 3).should be_true
+    end
+  end
+
+  context 'an impossible maze' do
+    it 'should be insoluble' do
+      maze = MazeSolver::MazeSolver.new(from_grid: <<-GRID
+********
+  **   *
+* **** *
+* *  * *
+* ** * *
+* **** *
+*      *
+********
+GRID
+      )
+      maze.solve(0, 1, 3, 3).should be_false
+    end
+  end
+
+  context 'from file' do
+    it 'should be creatable' do
+      maze = MazeSolver::MazeSolver.new(from_file: 'spec/test.maze')
+      maze.solve(18, 0, 12, 6).should be_true
+    end
   end
 
 end

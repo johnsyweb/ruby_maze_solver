8c2516e79fb793fb2f00295350852d3611939527
Pete Johns <paj-github@johnsy.com>
2013-12-23 19:30:40 +1100

    Remove ivars from spec

--- spec/maze_solver_spec.rb
+++ spec/maze_solver_spec.rb
@@ -2,60 +2,60 @@
 
 require 'spec_helper'
 
-describe MazeSolver do
+describe MazeSolver::MazeSolver do
   context 'a simple 8 x 3 maze' do
-    before do
-    @maze = MazeSolver::MazeSolver.new(from_grid: <<-GRID
+    let(:grid) do <<-GRID
 ********
 
 ********
 GRID
-      )
     end
 
-    specify { @maze.width.should eq 8 }
-    specify { @maze.height.should eq 3 }
+    subject(:maze) { described_class.new(from_grid: grid) }
+
+    its(:width) { should eq 8 }
+    its(:height) { should eq 3 }
 
     it 'should be possible to visit an empty space' do
-      @maze.should be_visitable(0, 1)
+      maze.should be_visitable(0, 1)
     end
 
     it 'should not be possible to visit a wall' do
-      @maze.should_not be_visitable(0, 0)
+      maze.should_not be_visitable(0, 0)
     end
 
     it 'should not be possible to visit outside the maze' do
-      @maze.should_not be_visitable(8, 3)
+      maze.should_not be_visitable(8, 3)
     end
 
     it 'should not be possible to visit a visited space' do
-      @maze.visit(0, 1)
-      @maze.should_not be_visitable(0, 1)
+      maze.visit(0, 1)
+      maze.should_not be_visitable(0, 1)
     end
 
     it 'should be possible to visit an unvisited space' do
-      @maze.visit(0, 1)
-      @maze.unvisit(0, 1)
-      @maze.should be_visitable(0, 1)
+      maze.visit(0, 1)
+      maze.unvisit(0, 1)
+      maze.should be_visitable(0, 1)
     end
 
     it 'should be soluble' do
-      @maze.solve(0, 1, 7, 1).should be_true
+      maze.solve(0, 1, 7, 1).should be_true
     end
 
     it 'should be soluble backwards' do
-      @maze.solve(7, 1, 0, 1).should be_true
+      maze.solve(7, 1, 0, 1).should be_true
     end
 
     it 'should be printable' do
-      @maze.to_s.should eq "********\n" +
+      maze.to_s.should eq "********\n" +
         "        \n" +
         "********\n"
     end
 
     it 'should be printable when solved' do
-      @maze.solve(0, 1, 7, 1)
-      @maze.to_s.should eq <<-GRID
+      maze.solve(0, 1, 7, 1)
+      maze.to_s.should eq <<-GRID
 ********
 ........
 ********
@@ -65,8 +65,8 @@ GRID
   end
 
   context 'a bigger maze' do
-    it 'should be soluble' do
-      maze = MazeSolver::MazeSolver.new(from_grid: <<-GRID
+    let(:grid) do
+      <<-GRID
 ********
   **   *
 * ** * *
@@ -75,15 +75,19 @@ GRID
 * **** *
 *      *
 ********
-GRID
-      )
+      GRID
+    end
+
+    subject(:maze) { described_class.new(from_grid: grid) }
+
+    it 'should be soluble' do
       maze.solve(0, 1, 3, 3).should be_true
     end
   end
 
   context 'an impossible maze' do
-    it 'should be insoluble' do
-      maze = MazeSolver::MazeSolver.new(from_grid: <<-GRID
+    let(:grid) do
+      <<-GRID
 ********
   **   *
 * **** *
@@ -93,14 +97,17 @@ GRID
 *      *
 ********
 GRID
-      )
+    end
+
+    subject(:maze) { described_class.new(from_grid: grid) }
+    it 'should be insoluble' do
       maze.solve(0, 1, 3, 3).should be_false
     end
   end
 
   context 'from file' do
-    before do
-      test_maze = StringIO.new(<<-DOC
+    let(:test_maze) do
+      StringIO.new(<<-DOC
 ****************** *
 *     *       **** *
 * ***** *****   ** *
@@ -108,10 +115,10 @@ GRID
 * *          ** ** *
 * ********** **    *
 ************ *******
-DOC
-      )
-      File.stub(:open).with('test.maze', 'r').and_return(test_maze)
+                   DOC
+                  )
     end
+    before { File.stub(:open).with('test.maze', 'r').and_return(test_maze) }
 
     it 'should be creatable' do
       maze = MazeSolver::MazeSolver.new(from_file: 'test.maze')

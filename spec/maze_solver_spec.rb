36a9c5b3f70b1327a0b94f1858a5634aabdea25d
Pete Johns <paj-github@johnsy.com>
2013-12-23 19:39:10 +1100

    s/should/expect/g

--- spec/maze_solver_spec.rb
+++ spec/maze_solver_spec.rb
@@ -16,52 +16,51 @@ GRID
     its(:width) { should eq 8 }
     its(:height) { should eq 3 }
 
-    it 'should be possible to visit an empty space' do
-      maze.should be_visitable(0, 1)
+    it 'is possible to visit an empty space' do
+      expect(maze).to be_visitable(0, 1)
     end
 
-    it 'should not be possible to visit a wall' do
-      maze.should_not be_visitable(0, 0)
+    it 'is not possible to visit a wall' do
+      expect(maze).to_not be_visitable(0, 0)
     end
 
-    it 'should not be possible to visit outside the maze' do
-      maze.should_not be_visitable(8, 3)
+    it 'is not possible to visit outside the maze' do
+      expect(maze).to_not be_visitable(8, 3)
     end
 
-    it 'should not be possible to visit a visited space' do
+    it 'is not possible to visit a visited space' do
       maze.visit(0, 1)
-      maze.should_not be_visitable(0, 1)
+      expect(maze).to_not be_visitable(0, 1)
     end
 
-    it 'should be possible to visit an unvisited space' do
+    it 'is possible to visit an unvisited space' do
       maze.visit(0, 1)
       maze.unvisit(0, 1)
-      maze.should be_visitable(0, 1)
+      expect(maze).to be_visitable(0, 1)
     end
 
-    it 'should be soluble' do
-      maze.solve(0, 1, 7, 1).should be_true
+    it 'is soluble' do
+      expect(maze.solve(0, 1, 7, 1)).to be_true
     end
 
-    it 'should be soluble backwards' do
-      maze.solve(7, 1, 0, 1).should be_true
+    it 'is soluble backwards' do
+      expect(maze.solve(7, 1, 0, 1)).to be_true
     end
 
-    it 'should be printable' do
-      maze.to_s.should eq "********\n" +
+    it 'is printable' do
+      expect(maze.to_s).to eq "********\n" +
         "        \n" +
         "********\n"
     end
 
-    it 'should be printable when solved' do
+    it 'is printable when solved' do
       maze.solve(0, 1, 7, 1)
-      maze.to_s.should eq <<-GRID
+      expect(maze.to_s).to eq <<-GRID
 ********
 ........
 ********
       GRID
     end
-
   end
 
   context 'a bigger maze' do
@@ -80,8 +79,8 @@ GRID
 
     subject(:maze) { described_class.new(from_grid: grid) }
 
-    it 'should be soluble' do
-      maze.solve(0, 1, 3, 3).should be_true
+    it 'is soluble' do
+      expect(maze.solve(0, 1, 3, 3)).to be_true
     end
   end
 
@@ -100,8 +99,9 @@ GRID
     end
 
     subject(:maze) { described_class.new(from_grid: grid) }
-    it 'should be insoluble' do
-      maze.solve(0, 1, 3, 3).should be_false
+
+    it 'is insoluble' do
+      expect(maze.solve(0, 1, 3, 3)).to be_false
     end
   end
 
@@ -118,11 +118,12 @@ GRID
                    DOC
                   )
     end
+
     before { File.stub(:open).with('test.maze', 'r').and_return(test_maze) }
 
-    it 'should be creatable' do
+    it 'is creatable' do
       maze = MazeSolver::MazeSolver.new(from_file: 'test.maze')
-      maze.solve(18, 0, 12, 6).should be_true
+      expect(maze.solve(18, 0, 12, 6)).to be_true
     end
   end
 end
